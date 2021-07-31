7f3a9b82e022e2b61b32adafe04810a2db9785ca
Igor Grkavac <grkavac.igor@gmail.com>
2021-07-31 11:32:50 +0200

    Parse arguments when starting the server

--- src/main.cpp
+++ src/main.cpp
@@ -20,17 +20,36 @@ namespace http = beast::http;     // from <boost/beast/http.hpp>
 namespace net = boost::asio;      // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp; // from <boost/asio/ip/tcp.hpp>
 
+bool has_only_digits(const std::string &s) {
+  return s.find_first_not_of("0123456789") == std::string::npos;
+}
+
+bool checkArguments(int argc, char *argv[]) {
+
+  if (argc != 5)
+    return false;
+
+  if (!has_only_digits(argv[2]) || !has_only_digits(argv[4]))
+    return false;
+
+  try {
+    net::ip::make_address(argv[1]);
+  } catch (const std::exception &) {
+    return false;
+  }
+
+  return true;
+}
+
 int main(int argc, char *argv[]) {
-  // Check command line arguments.
-  if (argc != 5) {
+
+  if (!checkArguments(argc, argv)) {
     std::cerr
         << "Usage: http-server-async <address> <port> <doc_root> <threads>\n"
         << "Example:\n"
         << "    http-server-async 0.0.0.0 8080 . 1\n";
     return EXIT_FAILURE;
   }
-
-  // TODO: Parse argv and check if server can be created
 
   Server server(argv);
 
