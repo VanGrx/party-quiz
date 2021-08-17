9ea0a03b6a3ec8e003c84bc3e56dd1cc4a34108b
Igor Grkavac <grkavac.igor@gmail.com>
2021-08-17 22:11:37 +0200

    Make server exit normally. Add websocket starter code

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,4 +1,14 @@
 #include "server.h"
+#include <boost/asio/bind_executor.hpp>
+#include <boost/asio/dispatch.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/make_unique.hpp>
+#include <boost/optional.hpp>
 #include <thread>
 
 Server::Server() {}
@@ -7,8 +17,7 @@ Server::Server(char *argv[])
     : threads{std::max<int>(1, std::atoi(argv[4]))},
       address{net::ip::make_address(argv[1])}, port{static_cast<unsigned short>(
                                                    std::atoi(argv[2]))},
-      doc_root{std::make_shared<std::string>(argv[3])},
-      ioc{std::max<int>(1, std::atoi(argv[4]))} {
+      doc_root{std::make_shared<std::string>(argv[3])}, ioc{threads} {
 
   // Create and launch a listening port
   connectionListener =
@@ -19,10 +28,24 @@ Server::Server(char *argv[])
 
 void Server::run() {
 
+  // Capture SIGINT and SIGTERM to perform a clean shutdown
+  net::signal_set signals(ioc, SIGINT, SIGTERM);
+  signals.async_wait([&](beast::error_code const &, int) {
+    // Stop the `io_context`. This will cause `run()`
+    // to return immediately, eventually destroying the
+    // `io_context` and all of the sockets in it.
+    ioc.stop();
+  });
   // Run the I/O service on the requested number of threads
   std::vector<std::thread> v;
   v.reserve(threads - 1);
   for (auto i = threads - 1; i > 0; --i)
     v.emplace_back([this] { ioc.run(); });
   ioc.run();
+
+  // (If we get here, it means we got a SIGINT or SIGTERM)
+
+  // Block until all the threads exit
+  for (auto &t : v)
+    t.join();
 }
