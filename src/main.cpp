3c5a187a36b36669ee73a6e0d5be04b2dcc034ef
Igor Grkavac <grkavac.igor@gmail.com>
2024-03-31 19:48:03 +0100

    Get address automatically

--- src/main.cpp
+++ src/main.cpp
@@ -21,33 +21,65 @@ bool has_only_digits(const std::string &s) {
 
 bool checkArguments(int argc, char *argv[]) {
 
-  if (argc != 5)
+  if (argc != 4)
     return false;
 
-  if (!has_only_digits(argv[2]) || !has_only_digits(argv[4]))
+  if (!has_only_digits(argv[1]) || !has_only_digits(argv[3]))
     return false;
-
-  try {
-    net::ip::make_address(argv[1]);
-  } catch (const std::exception &) {
-    return false;
-  }
 
   return true;
+}
+
+std::string getIPAddress() {
+  char buffer[1024];
+  struct sockaddr_in serv;
+  struct sockaddr_in name;
+  socklen_t namelen = sizeof(name);
+
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0) {
+    return "";
+  }
+
+  const char *google_dns_server = "8.8.8.8";
+  serv.sin_family = AF_INET;
+  serv.sin_addr.s_addr = inet_addr(google_dns_server);
+  serv.sin_port = htons(53);
+
+  if (connect(sockfd, (struct sockaddr *)&serv, sizeof(serv)) < 0) {
+    close(sockfd);
+    return "";
+  }
+
+  getsockname(sockfd, (struct sockaddr *)&name, &namelen);
+  close(sockfd);
+
+  const char *ip = inet_ntop(AF_INET, &name.sin_addr, buffer, sizeof(buffer));
+  if (ip == nullptr) {
+    return "";
+  }
+
+  return std::string(ip);
 }
 
 int main(int argc, char *argv[]) {
 
   if (!checkArguments(argc, argv)) {
-    std::cerr << "Usage: PartyQuiz <address> <port> <doc_root> <threads>\n"
+    std::cerr << "Usage: PartyQuiz <port> <doc_root> <threads>\n"
               << "Example:\n"
-              << "   PartyQuiz 0.0.0.0 8080 . 1\n";
+              << "   PartyQuiz 8080 . 1\n";
     return EXIT_FAILURE;
   }
 
-  auto threads = std::max<int>(1, std::atoi(argv[4]));
-  auto address = net::ip::make_address(argv[1]);
-  auto port = static_cast<unsigned short>(std::atoi(argv[2]));
+  auto ip_address_str = getIPAddress();
+
+  if (ip_address_str.empty()) {
+    std::cerr << "Error connecting to the internet\n";
+  }
+
+  auto threads = std::max<int>(1, std::atoi(argv[3]));
+  auto address = net::ip::make_address(ip_address_str);
+  auto port = static_cast<unsigned short>(std::atoi(argv[1]));
   auto doc_root = std::string(argv[3]);
 
   std::shared_ptr<Server> server = std::make_shared<Server>(threads, doc_root);
@@ -56,6 +88,8 @@ int main(int argc, char *argv[]) {
     std::cerr << "Init failed!\n";
     return EXIT_FAILURE;
   }
+
+  std::cout << "Listening on " << ip_address_str << "\n";
 
   server->start();
 
